393001969aad9501e27a4403d1ce659c6aedcd65
davidacalamaro <davidacalamaro@gmail.com>
2022-03-22 10:42:46 -0400

    default ui

---
+++ fordsUpVersion2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fordsUpVersion2
+//
+//  Created by david on 3/22/22.
+//
+
+import UIKit
+
+class SecondViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
